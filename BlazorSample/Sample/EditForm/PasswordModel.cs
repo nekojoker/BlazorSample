637e826b7488597f1b3779fe2504a5b0b218e374
nekojoker <nkjtky1@gmail.com>
2021-10-17 10:44:32 +0900

    同じ値の入力チェックを追加

---
+++ BlazorSample/Sample/EditForm/PasswordModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSample.Sample.EditForm
+{
+    public class PasswordModel
+    {
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "パスワードが一致しません。")]
+        public string ConfirmPassword { get; set; }
+    }
+}
