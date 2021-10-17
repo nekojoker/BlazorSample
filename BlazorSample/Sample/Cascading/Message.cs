bb448ae21c901036285e96606783d2b1cf4d8384
nekojoker <nkjtky1@gmail.com>
2021-10-17 12:28:27 +0900

    カスケーティングを追加

---
+++ BlazorSample/Sample/Cascading/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace BlazorSample.Sample.Cascading
+{
+    public class Message
+    {
+        private string _name;
+
+        public Message()
+        {
+            _name = "Guest";
+        }
+
+        public Message(string name)
+        {
+            _name = name;
+        }
+
+        public string GetText()
+        {
+            return $"Hello {_name}!!";
+        }
+    }
+}
